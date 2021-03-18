5ad8e3513132f97b4cd685ccf63e78f1dac3e8aa
André <andre_farao123@hotmail.com>
2021-03-17 21:53:50 -0300

    Initial setup and public assets

---
+++ netflix-web/src/components/jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Inner } from './styles/jumbotron';
+
+export const Jumbotron = ({}) => {
+  return (
+    <Inner>
+      {' '}
+      <p>Hello!</p>
+    </Inner>
+  );
+};
